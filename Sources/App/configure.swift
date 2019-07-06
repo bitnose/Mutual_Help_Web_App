098bf19a8960b80852b5af5274eba0936289457e
Sötnos <anniina.korkiakangas@icloud.com>
2019-07-06 16:06:07 +0300

    Add packages and do all the configurations, delete unneeded files

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,10 @@
-import FluentSQLite
 import Vapor
-
+import Leaf
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
-
+    try services.register(LeafProvider())
+    
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -13,20 +12,12 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     // Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+    middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    // Tells Vapor to use LeafRenderer when asked for a ViewRenderer type
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self) // 1
 
-    // Register the configured SQLite database to the database config.
-    var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
-
-    // Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
+   
 }
