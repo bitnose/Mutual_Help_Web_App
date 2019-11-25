288fe0b85b4780bde5fe80565e1793a7a450ff7b
Sötnos <anniina.korkiakangas@icloud.com>
2019-11-25 21:29:52 +0200

    Add some comments; Update the link in register.leaf

--- Sources/App/Utilities/Extensions/Date+ConvertToDateExtension.swift
+++ Sources/App/Utilities/Extensions/Date+ConvertToDateExtension.swift
@@ -8,22 +8,30 @@
 import Foundation
 
 
-/// # Extension to convert the Ad's date property to a date.
-
+/** # Extension to convert the Ad's date property to a date.
+ - parameters: Date
+ - returns: String
+ 
+ 1. This function formats Ad's date property to a date.
+ 2. Checking the operating system.
+ 3. Formatter object
+ 4. Add time zone (Central Europian TImezone)
+ 5. Using the date formatter, convert the date to a string.
+ 6. Return the date.
+ 7. If the formatter is not available, return "No".
+ */
 extension Date {
     
-    static func convertToDate(date: Date) -> String {
+    static func convertToDate(date: Date) -> String { // 1
         
-        if #available(OSX 10.12, *) {
-            let formatter = ISO8601DateFormatter()
-            formatter.timeZone = TimeZone.init(abbreviation: "CET")
+        if #available(OSX 10.12, *) { // 2
+            let formatter = ISO8601DateFormatter() // 3
+            formatter.timeZone = TimeZone.init(abbreviation: "CET") // 4
            
-            
-            let d = formatter.string(from: date)
-             print("\(d)")
-            return d
+            let d = formatter.string(from: date) // 5
+            return d // 6
     
-        } else {
+        } else { // 7
             print("Not available")
             return "No"
         }
